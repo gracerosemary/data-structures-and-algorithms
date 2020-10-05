666d05a7a59ccb5ce9f7dda673cc4d5de9306a59
Grace Choi <gracec@zillowgroup.com>
2020-10-04 19:02:43 -0700

    challenge 4 done

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -59,6 +59,9 @@ Write a function named sortByLength that takes in an array of strings and return
 
 const sortByLength = (arr) => {
   // Solution code here...
+  return arr.sort( (a,b) => {
+    return a.length - b.length;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
